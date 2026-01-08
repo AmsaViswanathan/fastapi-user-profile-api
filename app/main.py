a9a41d1018e54f69f558c21aa2af85d019116059
Amsa V <amsavisvanathan@gmail.com>
2026-01-08 21:51:04 +0530

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from .database import Base, engine
-from .api.routes import router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="User Profile Management API")
-app.include_router(router)
