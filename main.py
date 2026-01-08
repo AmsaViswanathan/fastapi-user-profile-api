5d15268caeb7dec0abefd5ae121832cafac1cb80
Amsa V <amsavisvanathan@gmail.com>
2026-01-08 22:56:34 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .database import Base, engine
-from .routes import router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Python Backend Assessment")
-
-app.include_router(router)
